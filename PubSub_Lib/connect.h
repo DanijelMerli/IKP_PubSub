72b4916689023d27d7b013e6104b8f4298d27832
Danijel Merli <danijel.merli@gmail.com>
2021-01-09 20:54:02 +0100

    windows socket initialization

--- PubSub_Lib/connect.h
+++ PubSub_Lib/connect.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <WinSock2.h>
+#include <stdio.h>
+
 #define TYPE_PUBLISHER 0
 #define TYPE_SUBSCRIBER 1
 
@@ -8,3 +11,5 @@
 // returns true if successful 
 // returns false if an error occured
 bool connect(int type);
+
+bool initializeWindowsSockets();
