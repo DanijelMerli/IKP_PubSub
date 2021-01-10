ef89187553a16448bd026a2be607a45bfbf55e23
Danijel Merli <danijel.merli@gmail.com>
2021-01-10 20:54:12 +0100

    added address and port defines

--- Common/connect.h
+++ Common/connect.h
@@ -1,15 +1,20 @@
 #pragma once
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
 #include <stdio.h>
 #include <WinSock2.h>
 
-#define TYPE_PUBLISHER 0
-#define TYPE_SUBSCRIBER 1
+#define SERVER_ADDR "127.0.0.1"
+#define PUB_PORT "10000"
+#define SUB_PORT "10001"
+#define PUB_PORT_INT 10000
+#define SUB_PORT_INT 10001
 
 // connect to PubSub Engine
-// type - type of connection (publisher/subscriber)
+// port - port of server socket (publisher-10000 / subscriber-10001)
 // returns true if successful 
 // returns false if an error occured
-bool connect(int type);
+bool connect(PCSTR port);
 
 // Initialize windows sockets library
 bool initializeWindowsSockets(); 
